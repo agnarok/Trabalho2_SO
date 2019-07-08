41923c5a5513a0947d9854b622ed4d04a5187ad8
Agnarok <abj@icomp.ufam.edu.br>
2019-07-07 20:59:10 -0400

    [UPD] Estrutura de dados implementada na questao 3
    * Pedro é corno

--- Questao 3/questao3.cpp
+++ Questao 3/questao3.cpp
@@ -5,22 +5,40 @@
 #include <unistd.h>
 using namespace std;
 
+#define tam 5
+
 int main()
 {
     omp_lock_t lock;
     omp_init_lock(&lock);
-    int vet[2];
-    omp_set_num_threads(10);
+    int vet[tam];
+    for(int i=0 ; i<tam;i++){
+        vet[i]=0;
+    }
+    omp_set_num_threads(tam*2);
+    int rand_numbers[tam*2];
+    cout<< "Indexação: ";
+    for(int i=0;i<tam*2;i++){
+        rand_numbers[i] = rand() % tam;
+        cout<< rand_numbers[i]<<" ";
+    }
+    cout << endl;
+    int aux;
 #pragma omp parallel
     {   
         cout << "Thread " << omp_get_thread_num() << " inizializando" << endl;
         omp_set_lock(&lock);
         cout << "Thread " << omp_get_thread_num() << " executando" << endl;
-        sleep(20);
-        vet[0] = omp_get_thread_num();
-        cout << "Thread " << omp_get_thread_num() << " finalizada" << endl;
+        aux = vet[rand_numbers[omp_get_thread_num()]]+1; 
+        sleep(1);
+        vet[rand_numbers[omp_get_thread_num()]] =aux;
         omp_unset_lock(&lock);
+        cout << "Thread " << omp_get_thread_num() << " finalizada" << endl;
         
     }
-    cout << vet[0] << endl;
+    cout<< "Vetor: ";
+    for(int i=0;i<tam;i++){
+        cout<< vet[i]<<" ";
+    }
+    cout << endl;
 }
--- Questao 3/questao3parallel.cpp
+++ Questao 3/questao3parallel.cpp
@@ -5,18 +5,36 @@
 #include <unistd.h>
 using namespace std;
 
+#define tam 5
+
 int main()
 {
-    int vet[2];
-    omp_set_num_threads(10);
+    int vet[tam];
+    for(int i=0 ; i<tam;i++){
+        vet[i]=0;
+    }
+    omp_set_num_threads(tam*2);
+    int rand_numbers[tam*2];
+    cout<< "Indexação: ";
+    for(int i=0;i<tam*2;i++){
+        rand_numbers[i] = rand() % tam;
+        cout<< rand_numbers[i]<<" ";
+    }
+    cout << endl;
+    int aux;
 #pragma omp parallel
     {   
         cout << "Thread " << omp_get_thread_num() << " inizializando" << endl;
-
         cout << "Thread " << omp_get_thread_num() << " executando" << endl;
-        vet[0] = omp_get_thread_num();
+        aux = vet[rand_numbers[omp_get_thread_num()]]+1; 
+        sleep(1);
+        vet[rand_numbers[omp_get_thread_num()]] =aux;
         cout << "Thread " << omp_get_thread_num() << " finalizada" << endl;
         
     }
-    cout << vet[0] << endl;
+    cout<< "Vetor: ";
+    for(int i=0;i<tam;i++){
+        cout<< vet[i]<<" ";
+    }
+    cout << endl;
 }
